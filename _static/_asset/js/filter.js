440e651e291320b57f7add0f10a054c10f10984c
damoncard <cardtergt@hotmail.com>
2017-04-19 18:53:20 +0700

    Done filter by company

--- _static/_asset/js/filter.js
+++ _static/_asset/js/filter.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 	$.ajax({
 		type: 'POST',
 		url: '/fnc/filter/main.php',
-		data: {'position': 'none', 'province': 'none', 'gender': 'none'},
+		data: {'position': 'none', 'province': 'none', 'gender': 'none', 'company': 'none'},
 		dataType: "json",
 		success: function (data) {
 			$('#filter-table tr').slice(1).empty()
@@ -23,11 +23,16 @@ $(document).ready(function() {
 		var pos = $('#position').val()
 		var pro = $('#province').val()
 		var gen = $('#gender').val()
+		var com = $('#company').val()
+
+		if (com == '') {
+			com = 'none'
+		}
 
 		$.ajax({
 			type: 'POST',
 			url: '/fnc/filter/main.php',
-			data: {'position': pos, 'province': pro, 'gender': gen},
+			data: {'position': pos, 'province': pro, 'gender': gen, 'company': com},
 			dataType: "json",
 			success: function (data) {
 				$('#filter-table tr').slice(1).empty()
@@ -40,6 +45,18 @@ $(document).ready(function() {
 						<td><button class="btn btn-info">more detail</button></td>
 					</tr>`)
 				}
+			}
+		})
+	})
+
+	$('#company').keyup(function() {
+		$('#filter-table > tr').each(function(){
+			var str = $(this).find('td:nth-child(3)').text().toLowerCase()
+			var sub = $('#company').val().toLowerCase()
+			if (str.indexOf(sub)){
+				$(this).hide()
+			} else {
+				$(this).show()
 			}
 		})
 	})
