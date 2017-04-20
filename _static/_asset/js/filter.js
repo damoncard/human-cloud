4fb9951b24a22828a33428aa74928dd90053a354
damoncard <cardtergt@hotmail.com>
2017-04-20 20:51:26 +0700

    Add non-function flip-card & Modify filtering (Gender & Province require)

--- _static/_asset/js/filter.js
+++ _static/_asset/js/filter.js
@@ -1,49 +1,76 @@
-@@ -1,47 +0,0 @@
 $(document).ready(function() {
-	$('#filter-table').DataTable()
-
 	$.ajax({
 		type: 'POST',
 		url: '/fnc/filter/main.php',
-		data: {'position': 'none', 'province': 'none', 'gender': 'none'},
+		data: {'position': 'none', 'province': 'none', 'gender': 'none', 'company': 'none'},
 		dataType: "json",
 		success: function (data) {
-			$('#filter-table tr').slice(1).empty()
+			$('#profile').empty()
 			for (var i = 0; i < data.length; i++) { 
-				$('#filter-table  > tbody:last-child').append(`		
-				<tr>
-					<td>` + data[i]['m_FirstName'] + `</td>
-					<td>` + data[i]['position'] + `</td>
-					<td>` + data[i]['company'] + `</td>
-					<td><button class="btn btn-info">more detail</button></td>
-				</tr>`)
+				$('#profile').append(`
+				<div class="col-md-4 wow fadeInRight delay-02s">
+					<div class="block" id="card"> 
+						<div class="thumbnail front"><br>
+							<img src="_asset/img/img1.png" alt="" class="img-responsive">
+							<div class="caption">
+								<h5 style="font-weight: bold;">NAME: 
+									<h6>` + data[i]['m_FirstName']  + `</h6>
+								</h5>
+								<h5 style="font-weight: bold;">POSITION:
+									<h6>` +  data[i]['position']  + `</h6>
+								</h5>
+								<h5 style="font-weight: bold;">COMPANY:
+									<h6>` +  data[i]['company']  + `</h6>
+								</h5>
+							</div>
+						</div>
+						<div class="thumbnail back"><br>
+							<img src="_asset/img/img1.png" alt="" class="img-responsive">
+						</div>
+					</div>
+				</div>`)
 			}
 		}
 	})
 
-	$('select').change(function(){
-		var pos = $('#position').val()
-		var pro = $('#province').val()
-		var gen = $('#gender').val()
+	// $(function($) {
+	// 	$("#card").flip()
+	// });
 
-		$.ajax({
-			type: 'POST',
-			url: '/fnc/filter/main.php',
-			data: {'position': pos, 'province': pro, 'gender': gen},
-			dataType: "json",
-			success: function (data) {
-				$('#filter-table tr').slice(1).empty()
-				for (var i = 0; i < data.length; i++) { 
-					$('#filter-table  > tbody:last-child').append(`		
-					<tr>
-						<td>` + data[i]['m_FirstName'] + `</td>
-						<td>` + data[i]['position'] + `</td>
-						<td>` + data[i]['company'] + `</td>
-						<td><button class="btn btn-info">more detail</button></td>
-					</tr>`)
-				}
-			}
-		})
+	$('select').change(function(){
+		var key = $(this).val()
+		if ($(this).attr('id') == 'position') {
+			$('#profile > div').each(function(){
+	 			var str = $(this).find('h6:nth-child(4)').text()
+	 			var sub = key
+	 			if (str != sub){
+	 				$(this).hide()
+	 			} else {
+	 				$(this).show()
+	 			}
+	 		})
+		} else {
+			/*$('#profile > div').each(function(){
+	 			var str = $(this).find('h6:nth-child(4)').text()
+	 			var sub = key
+	 			if (str != sub){
+	 				$(this).hide()
+	 			} else {
+	 				$(this).show()
+	 			}
+	 		})*/	
+		}
 	})
+
+	$('#company').keyup(function() {
+ 		$('#profile > div').each(function(){
+ 			var str = $(this).find('h6:last-child').text().toLowerCase()
+ 			var sub = $('#company').val().toLowerCase()
+ 			if (str.indexOf(sub)){
+ 				$(this).hide()
+ 			} else {
+ 				$(this).show()
+ 			}
+ 		})
+ 	})
 })
-\ No newline at end of file
